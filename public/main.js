f40365ade238ba40a496ff6465098f19f3d07729
Cristian <ccinetto@edrans.com>
2021-08-18 14:34:33 -0300

    actualizado proyecto

--- public/main.js
+++ public/main.js
@@ -1,31 +1,73 @@
 const socket = io.connect();
+const nombre = document.getElementById('nombre');
+const precio = document.getElementById('price');
+const imagen = document.getElementById('thumbnail');
 
-function sendMsg (e) {
-    let mensaje = {
-        nombre: document.getElementById('nombre').value,
-        precio: document.getElementById('precio').value,
-        imagen: document.getElementById('imagen').value,
-    }
-    socket.emit('new-message', mensaje);
+console.log(nombre);
+console.log(precio);
+console.log(imagen);
+function sendMsg(e) {
+  let mensaje = {
+    nombre: nombre.value,
+    precio: precio.value,
+    imagen: imagen.value,
+  };
+  socket.emit('new-message', mensaje);
 }
 
 function render(data) {
-    var html = data
-        .map(function (elem, index){
-            return `<div>
+  var html = data
+    .map(function (elem, index) {
+      return `<div>
                         <strong>${elem.nombre}</strong>
                         <em>${elem.precio}</em>
                         <em>${elem.imagen}</em>
                     </div>`;
-        })
-        .join (' ');
+    })
+    .join(' ');
 
-        document.getElementById('messages').innerHTML = html;
+  document.getElementById('messages').innerHTML = html;
 }
 
-socket.on('messages',  (data) => {
-    console.log('Recibi Mensaje');
- 
-    render(data);
+socket.on('messages', (data) => {
+  console.log('Recibi Mensaje');
 
+  render(data);
+});
+
+const form = document.getElementById('miForm');
+
+form.addEventListener('submit', function (e) {
+  console.log('HOLA');
+  e.preventDefault();
+
+  console.log('HACER AQUI EL POST REQUEST USANDO FETCH');
+  const data = {
+    title: nombre.value,
+    price: precio.value,
+    thumbnail: imagen.value,
+  };
+
+  const options = {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  };
+
+  fetch('/api/productos/guardar', options)
+    .then((data) => {
+      console.log('TODO BIEN');
+      console.log(data);
+      console.log('LUEGO ACTUALIZAR LA INFO DE LA PAGINA AQUI');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      return false;
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import express  from 'express';
+import express from 'express';
 import path from 'path';
 import handlebars from 'express-handlebars';
-import routesproduct from '/Users/virgi tourn/OneDrive/Escritorio/coder backend/clase 10/ruta/routesproduct';
+import routesproduct from './ruta/routesproduct';
 import * as http from 'http';
-import io from 'socket.io;'
-
+import io from 'socket.io';
 
 const app = express();
-const puerto = 5080;
+const puerto = 8080;
 
-const server = app.listen(puerto, () =>
-  console.log('SERVER UP en puerto', puerto)
-);
+app.use(express.json());
 
 const publicFolderPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicFolderPath));
@@ -20,15 +17,14 @@ const layoutFolderPath = path.resolve(__dirname, '../views/layouts');
 const defaultLayerPth = path.resolve(__dirname, '../views/layouts/index');
 const partialsDirPath = path.resolve(__dirname, '../views/partials');
 
-
 app.set('view engine', 'handlebars');
 app.engine(
   'handlebars',
   handlebars({
     layoutDir: layoutFolderPath,
     defaultLayout: defaultLayerPth,
-     partialsDir: partialsDirPath,
-    })
+    partialsDir: partialsDirPath,
+  })
 );
 
 const myServer = http.Server(app);
@@ -36,29 +32,25 @@ myServer.listen(puerto, () => console.log('SERVER UP en puerto', puerto));
 const myWSServer = io(myServer);
 const messages = [];
 
-myWSServer.on('conecction', (socket) => {
-    console.log('Cliente Conectado');
-    console.log(`Cliente SID => ${socket.client.id}`);
-    console.log(`SERVER SID ===> ${socket.id}`);
+myWSServer.on('connection', (socket) => {
+  console.log('Cliente Conectado');
+  console.log(`Cliente SID => ${socket.client.id}`);
+  console.log(`SERVER SID ===> ${socket.id}`);
 
-    socket.on('new-message',  (data) => {
-        console.log('RECIBI UN MSJ NUEVO');
-        console.log(data);
-        messages.push(data);
-        socket.emit('messages',messages);
-       });
+  socket.on('new-message', (data) => {
+    console.log('RECIBI UN MSJ NUEVO');
+    console.log(data);
+    messages.push(data);
+    socket.emit('messages', messages);
+  });
 });
 
 app.get('/', (req, res) => {
   res.render('main', { layout: 'index' });
 });
 
-app.use("./api/productos", routesproduct);
-
-
+app.use('/api/productos', routesproduct);
 
 app.get('/', (req, res) => {
-  
-  res.sendFile (__dirname + './public/html.html');
-}
-);
+  res.sendFile(__dirname + './public/html.html');
+});
